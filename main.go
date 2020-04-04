e34d474fc7cb1c86291b047639d9ad8ca75fba1a
yangming <314504857@qq.com>
2020-04-04 18:15:54 +0800

    add negroni

--- main.go
+++ main.go
@@ -1,18 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"github.com/unrolled/render"
+	"github.com/urfave/negroni"
 	"net/http"
+	"time"
 )
 
+var formatter = render.New()
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Accept request from ", r.RemoteAddr)
-		fmt.Fprint(w, "Hello from go.\n")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		//fmt.Println("Accept request from ", r.RemoteAddr)
+		formatter.Text(w, http.StatusOK, "Hello from go")
+		return
+	})
+	mux.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
+		//fmt.Println("Accept request from ", r.RemoteAddr)
+		formatter.JSON(w, http.StatusOK, struct {
+			Author string
+		}{"yang ming"})
 		return
 	})
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	n := negroni.Classic()
+	n.UseHandler(mux)
+
+	server := &http.Server{
+		Addr:           ":8080",
+		Handler:        n,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	if err := server.ListenAndServe(); err != nil {
 		panic(err)
 	}
 }
