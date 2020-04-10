262721b7e53636de565039f2925e509c4aaa9f87
yangming <314504857@qq.com>
2020-04-10 09:03:50 +0800

    shutdown server

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/cnyangming/webservice/router"
 	"github.com/urfave/negroni"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
 )
 
@@ -21,8 +25,24 @@ func main() {
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-	fmt.Println("Start service: http://127.0.0.1:8080")
-	if err := server.ListenAndServe(); err != nil {
-		panic(err)
+
+	go func() {
+		fmt.Println("Start service: http://127.0.0.1:8080")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalln("Listen: ", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("ShutDown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Println("Server ShutDown: ", err)
 	}
+	log.Println("Server Exiting.")
 }
